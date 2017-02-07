129b723c62a38621158bfcdea016f8e2918b392c
Leeingnyo <dyd1928@naver.com>
2017-02-08 05:39:13 +0900

    make components, bind event

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.handleClickSubtractButton = this.handleClickSubtractButton.bind(this);
+    this.handleClickAddButton = this.handleClickAddButton.bind(this);
+  }
+
+  handleClickSubtractButton() {
+    // - action 발생시키기
+  }
+
+  handleClickAddButton() {
+    // + action 발생시키기
+  }
+
   render() {
+    const value = 0;
     return (
-      <h1>React Seminar 3<small> - Redux</small></h1>
+      <div>
+        <button onClick={this.handleClickSubtractButton}>-</button>
+        <span>{value}</span>
+        <button onClick={this.handleClickAddButton}>+</button>
+      </div>
     );
   }
 }
