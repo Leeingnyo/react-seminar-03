02d603553eb704f29ba84ddf33151cf8b8dc2652
Leeingnyo <dyd1928@naver.com>
2017-02-08 06:31:25 +0900

    make store

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import App from './components/App';
+import reducers from './reducers';
+
+const store = createStore(reducers);
+// 만든 리듀서로 스토어 생성
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  // Provider 로 모든 앱에 제공
+  // connect에 쓰임
   document.getElementById('root')
 );
