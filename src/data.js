441477dac95763e9fca61baf29d66381db23a1ef
Leeingnyo <dyd1928@naver.com>
2017-02-08 02:47:44 +0900

    add dummy items

---
+++ src/data.js
@@ -0,0 +1,202 @@
+export const dummyItems = [
+  {
+    name: 'alphabet0',
+    img: 'https://images.template.net/wp-content/uploads/2015/06/Bright-Colorful-Printable-Alphabets.jpg',
+    hashtags: [
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 's', 'q', 'u', 'v', 'w', 'z', 'y', 'x', 'r', 't'
+    ],
+  },
+  {
+    name: 'alphabet1',
+    img: 'https://images.template.net/wp-content/uploads/2015/06/Bright-Colorful-Printable-Alphabets.jpg',
+    hashtags: [
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 's', 'q', 'u', 'v', 'w', 'z', 'y', 'x', 'r', 't'
+    ],
+  },
+  {
+    name: 'alphabet2',
+    img: 'https://images.template.net/wp-content/uploads/2015/06/Bright-Colorful-Printable-Alphabets.jpg',
+    hashtags: [
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 's', 'q', 'u', 'v', 'w', 'z', 'y', 'x', 'r', 't'
+    ],
+  },
+  {
+    name: 'alphabet3',
+    img: 'https://images.template.net/wp-content/uploads/2015/06/Bright-Colorful-Printable-Alphabets.jpg',
+    hashtags: [
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 's', 'q', 'u', 'v', 'w', 'z', 'y', 'x', 'r', 't'
+    ],
+  },
+  {
+    name: 'alphabet4',
+    img: 'https://images.template.net/wp-content/uploads/2015/06/Bright-Colorful-Printable-Alphabets.jpg',
+    hashtags: [
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 's', 'q', 'u', 'v', 'w', 'z', 'y', 'x', 'r', 't'
+    ],
+  },
+  {
+    name: 'alphabet5',
+    img: 'https://images.template.net/wp-content/uploads/2015/06/Bright-Colorful-Printable-Alphabets.jpg',
+    hashtags: [
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 's', 'q', 'u', 'v', 'w', 'z', 'y', 'x', 'r', 't'
+    ],
+  },
+  {
+    name: 'alphabet6',
+    img: 'https://images.template.net/wp-content/uploads/2015/06/Bright-Colorful-Printable-Alphabets.jpg',
+    hashtags: [
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 's', 'q', 'u', 'v', 'w', 'z', 'y', 'x', 'r', 't'
+    ],
+  },
+  {
+    name: 'alphabet7',
+    img: 'https://images.template.net/wp-content/uploads/2015/06/Bright-Colorful-Printable-Alphabets.jpg',
+    hashtags: [
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 's', 'q', 'u', 'v', 'w', 'z', 'y', 'x', 'r', 't'
+    ],
+  },
+  {
+    name: 'alphabet8',
+    img: 'https://images.template.net/wp-content/uploads/2015/06/Bright-Colorful-Printable-Alphabets.jpg',
+    hashtags: [
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 's', 'q', 'u', 'v', 'w', 'z', 'y', 'x', 'r', 't'
+    ],
+  },
+  {
+    name: 'alphabet9',
+    img: 'https://images.template.net/wp-content/uploads/2015/06/Bright-Colorful-Printable-Alphabets.jpg',
+    hashtags: [
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 's', 'q', 'u', 'v', 'w', 'z', 'y', 'x', 'r', 't'
+    ],
+  },
+  {
+    name: 'alphabet10',
+    img: 'https://images.template.net/wp-content/uploads/2015/06/Bright-Colorful-Printable-Alphabets.jpg',
+    hashtags: [
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 's', 'q', 'u', 'v', 'w', 'z', 'y', 'x', 'r', 't'
+    ],
+  },
+  {
+    name: 'alphabet11',
+    img: 'https://images.template.net/wp-content/uploads/2015/06/Bright-Colorful-Printable-Alphabets.jpg',
+    hashtags: [
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 's', 'q', 'u', 'v', 'w', 'z', 'y', 'x', 'r', 't'
+    ],
+  },
+  {
+    name: 'alphabet12',
+    img: 'https://images.template.net/wp-content/uploads/2015/06/Bright-Colorful-Printable-Alphabets.jpg',
+    hashtags: [
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 's', 'q', 'u', 'v', 'w', 'z', 'y', 'x', 'r', 't'
+    ],
+  },
+  {
+    name: 'alphabet13',
+    img: 'https://images.template.net/wp-content/uploads/2015/06/Bright-Colorful-Printable-Alphabets.jpg',
+    hashtags: [
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 's', 'q', 'u', 'v', 'w', 'z', 'y', 'x', 'r', 't'
+    ],
+  },
+  {
+    name: 'alphabet14',
+    img: 'https://images.template.net/wp-content/uploads/2015/06/Bright-Colorful-Printable-Alphabets.jpg',
+    hashtags: [
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 's', 'q', 'u', 'v', 'w', 'z', 'y', 'x', 'r', 't'
+    ],
+  },
+  {
+    name: '치즈라면',
+    img: 'http://www.dogdrip.net/files/attach/images/37396208/743/523/051/2cc4a76fb5fb0ed8b3b9a8229ebb784d.PNG',
+    hashtags: [
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 's', 'q', 'u', 'v', 'w', 'z', 'y', 'x', 'r', 't'
+    ],
+  },
+  {
+    name: '라면',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Ramyeon.jpg/250px-Ramyeon.jpg',
+    hashtags: [
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 's', 'q', 'u', 'v', 'w', 'z', 'y', 'x', 'r', 't'
+    ],
+  },
+  {
+    name: 'alphabet17',
+    img: 'https://images.template.net/wp-content/uploads/2015/06/Bright-Colorful-Printable-Alphabets.jpg',
+    hashtags: [
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 's', 'q', 'u', 'v', 'w', 'z', 'y', 'x', 'r', 't'
+    ],
+  },
+  {
+    name: 'alphabet18',
+    img: 'https://images.template.net/wp-content/uploads/2015/06/Bright-Colorful-Printable-Alphabets.jpg',
+    hashtags: [
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 's', 'q', 'u', 'v', 'w', 'z', 'y', 'x', 'r', 't'
+    ],
+  },
+  {
+    name: 'alphabet19',
+    img: 'https://images.template.net/wp-content/uploads/2015/06/Bright-Colorful-Printable-Alphabets.jpg',
+    hashtags: [
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 's', 'q', 'u', 'v', 'w', 'z', 'y', 'x', 'r', 't'
+    ],
+  },
+  {
+    name: 'react0',
+    img: 'https://facebook.github.io/react/img/logo.svg',
+    hashtags: [
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 's', 'q', 'u', 'v', 'w', 'z', 'y', 'x', 'r', 't'
+    ],
+  },
+  {
+    name: 'react1',
+    img: 'https://facebook.github.io/react/img/logo.svg',
+    hashtags: [
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 's', 'q', 'u', 'v', 'w', 'z', 'y', 'x', 'r', 't'
+    ],
+  },
+  {
+    name: 'react2',
+    img: 'https://facebook.github.io/react/img/logo.svg',
+    hashtags: [
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 's', 'q', 'u', 'v', 'w', 'z', 'y', 'x', 'r', 't'
+    ],
+  },
+  {
+    name: 'wondergirls',
+    img: 'http://www.abstractfonts.com/forum/images/5d7a3f5f497113c76cee17b40abddadb.jpg',
+    hashtags: [
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 's', 'q', 'u', 'v', 'w', 'z', 'y', 'x', 'r', 't'
+    ],
+  },
+  {
+    name: 'amcg',
+    img: 'http://img14.deviantart.net/3e85/i/2013/361/d/e/the_idolm_ster___amcg_logo_by_thewolfbunny-d6zm3x7.png',
+    hashtags: [
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 's', 'q', 'u', 'v', 'w', 'z', 'y', 'x', 'r', 't'
+    ],
+  },
+];
