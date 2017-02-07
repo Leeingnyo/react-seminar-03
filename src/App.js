b6f6776a034a903403be1382c06a30b82c60afb2
Leeingnyo <dyd1928@naver.com>
2017-02-07 23:24:56 +0900

    move mode state of Main to App

--- src/App.js
+++ src/App.js
@@ -3,15 +3,28 @@ import React, { Component } from 'react';
 import TopMenu from './containers/TopMenu';
 import SideMenu from './containers/SideMenu';
 import Main from './containers/Main';
+import {
+  SEARCH_MODE,
+  MY_SELECTION_MODE,
+  TRASH_BIN_MODE,
+} from './const';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mode: SEARCH_MODE,
+    };
+  }
 
   render() {
     return (
       <div id="wrapper">
         <div id="main-wrapper">
           <TopMenu />
-          <Main />
+          <Main mode={this.state.mode}/>
         </div>
         <SideMenu />
       </div>
--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,31 +1,28 @@
 import React, { Component } from 'react';
 
+import {
+  SEARCH_MODE,
+  MY_SELECTION_MODE,
+  TRASH_BIN_MODE,
+} from '../const';
 import Search from './Search';
 import MySelection from './MySelection';
 import TrashBin from './TrashBin';
-
-const SEARCH_MODE = 0;
-const MY_SELEcTION_MODE = 1;
-const TRASH_BIN_MODE = 2;
 
 class Main extends Component {
 
   constructor(props) {
     super(props);
-
-    this.state = {
-      mode: SEARCH_MODE,
-    };
   }
 
   render() {
     let view = null;
 
-    switch (this.state.mode) {
+    switch (this.props.mode) {
       case SEARCH_MODE:
         view = <Search />;
         break;
-      case MY_SELEcTION_MODE:
+      case MY_SELECTION_MODE:
         view = <MySelection />;
         break;
       case TRASH_BIN_MODE:
