89ca0a4c6604727915e197014b9c5f61bd7e88fc
Leeingnyo <dyd1928@naver.com>
2017-02-07 20:05:42 +0900

    extract main app

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>React Seminar 3<small> - Redux</small></h1>
+    );
+  }
+}
+
+export default App;
