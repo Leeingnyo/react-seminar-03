204c3692bc7935f3a4ec01dc73121a14d44a5d95
Leeingnyo <dyd1928@naver.com>
2017-02-08 03:56:05 +0900

    implement isRegistered

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -7,7 +7,11 @@ class ItemList extends Component {
   render() {
     const {items} = this.props;
     const listItems = items.map(item => {
-      return <Item key={item.name} item={item} isRegistered={false} register={this.props.register} deregister={this.props.deregister} />;
+      let isRegistered = false;
+      this.props.registered.forEach(registeredItem => {
+        isRegistered = isRegistered || item.name === registeredItem.name;
+      });
+      return <Item key={item.name} item={item} isRegistered={isRegistered} register={this.props.register} deregister={this.props.deregister} />;
       // 넘겨받은 registered에 속하는 지 보고 isRegistered 를 설정한다
         // 이거 넘겨주는 거 잊어버려서 다시 따라가서 줘야한다
       // 받은 register, deregister를 또또 넘겨주고 있다
